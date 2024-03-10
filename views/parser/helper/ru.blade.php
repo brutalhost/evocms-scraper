e3a41f59a92406b71d148fd50312e2b5fcd2a162
Andrey Orlov <noiselassie@outlook.com>
2024-03-10 20:59:20 +0400

    Update ru.blade.php

--- views/parser/helper/ru.blade.php
+++ views/parser/helper/ru.blade.php
@@ -3,20 +3,20 @@
 <p class="has-line-data" data-line-start="2" data-line-end="3">Список классов находится в файле конфигурации <code>/project/core/custom/configs/scraper.php</code>.</p>
 <p class="has-line-data" data-line-start="4" data-line-end="5">Вы можете как создавать классы с нуля, так и наследоваться от <code>EvolutionCMS\Scraper\Services\Parsers\AbstractParser</code>. Для лучшего понимания,
     изучите исходные файлы парсеров, поставляемых с модулем.</p>
-<h3 class="code-line" data-line-start=5 data-line-end=6><a id="__5"></a>Обработка задач</h3>
+<h3 class="code-line" data-line-start=5 data-line-end=6><a id="__5"></a>Обработка заданий</h3>
 <p class="has-line-data" data-line-start="6" data-line-end="8"><code>php artisan scraper:process {task?}
         {--with-completed} { --with-unfinished} {--ignore-timestamp}</code><br>
-    Начинает обработку задачи со статусом Created.</p>
+    Начинает обработку задания со статусом Created.</p>
 <p class="has-line-data" data-line-start="9" data-line-end="11"><code>php artisan scraper:process 13</code><br>
-    Запустить обработку задачи с идентификатором 13, даже если она уже обработана.</p>
+    Запустить обработку задания с идентификатором 13, даже если она уже обработана.</p>
 <p class="has-line-data" data-line-start="12" data-line-end="14"><code>php artisan scraper:process
         --with-completed</code><br>
-    Обработка задачи со статусом Created или Completed.</p>
+    Обработка задания со статусом Created или Completed.</p>
 <p class="has-line-data" data-line-start="15" data-line-end="17"><code>php artisan scraper:process --with-unfinished
         --with-completed</code><br>
-    Обработка задачи со статусом Created, Unfinished или Completed.</p>
+    Обработка задания со статусом Created, Unfinished или Completed.</p>
 <p class="has-line-data" data-line-start="18" data-line-end="20"><code>php artisan scraper:process
         --ignore-timestamp</code><br>
     Игнорирует поле метки времени.</p>
 <p class="has-line-data" data-line-start="21" data-line-end="23"><code>php artisan scraper:mark-tasks</code><br>
-    Изменяет статус на Created для всех задач Completed, к которым не прикреплены документы.</p>
+    Изменяет статус на Created для всех заданий Completed, к которым не прикреплены документы.</p>
